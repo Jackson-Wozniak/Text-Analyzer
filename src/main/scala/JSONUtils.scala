266965f375593c365cb9f7dffd41a864ec341dea
Jackson Wozniak <jjw30@comcast.net>
2024-08-06 15:40:11 -0400

    display JSON map in sorted order for histograms

--- src/main/scala/JSONUtils.scala
+++ src/main/scala/JSONUtils.scala
@@ -7,7 +7,7 @@ object JSONUtils {
     def toJSON(indentOffset: Int = 0): String =
       val indentStr = " " * (indentOffset + 4)
       val initialIndent = " " * indentOffset
-      val entries = map.map((key, value) => s"""$indentStr"$key" : $value""").mkString(",\n")
+      val entries = map.toSeq.sortBy(-_._2).map((key, value) => s"""$indentStr"$key" : $value""").mkString(",\n")
       s"{\n$entries\n$initialIndent}"
   }
 }
