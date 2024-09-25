c6db0c6984fdee1d5a3b83f1b8fbe85653538078
Jackson Wozniak <jjw30@comcast.net>
2024-09-25 13:56:08 -0400

    add makefile and char map structs

---
+++ C-version/text_file.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "text_file.h"
--- C-version/text_file.h
+++ C-version/text_file.h
@@ -2,3 +2,19 @@
 #define INPUT_DIRECTORY "../input_files/files/"
 #define OUTPUT_MANAGER_PATH "output/output_names.txt"
 #define OUTPUT_DIRECTORY "output/"
+
+typedef struct CHAR_MAP_ENTRY{
+    char* key;
+    int value;
+} CHAR_MAP_ENTRY;
+
+typedef struct CHAR_MAP{
+    int size;
+    struct CHAR_MAP_ENTRY* entries;
+} CHAR_MAP;
+
+typedef struct TEXT_FILE{
+    int line_count;
+    int word_count;
+    struct CHAR_MAP* char_histogram;
+} TEXT_FILE;
