ebe5f4e808fa83e56d9c677fbfe200eb110bbff9
Jackson Wozniak <jjw30@comcast.net>
2024-09-25 13:37:34 -0400

    restructure folder, and create a C version of the analyzer alongside the current scala implementation

---
+++ C-version/file_runner.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "text_file.h"
+
+int main(int argc, char* argv[]){
+    return 0;
+}
---
+++ C-version/text_file.h
@@ -0,0 +1,4 @@
+#define INPUT_MANAGER_PATH "../input_files/input_files.txt"
+#define INPUT_DIRECTORY "../input_files/files/"
+#define OUTPUT_MANAGER_PATH "output/output_names.txt"
+#define OUTPUT_DIRECTORY "output/"
