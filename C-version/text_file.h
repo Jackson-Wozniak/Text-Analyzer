8cf4aae184202f05ef334bfa52a7dfe9c9b322eb
Jackson Wozniak <jjw30@comcast.net>
2024-10-05 10:15:20 -0400

    restructure folders

--- C-version/text_file.h
+++ C-version/text_file.h
@@ -1,5 +1,6 @@
-#define INPUT_MANAGER_PATH "../input_files/input_files.txt"
-#define INPUT_DIRECTORY "../input_files/files/"
+// #define INPUT_MANAGER_PATH "../input_files/input_files.txt"
+#define INPUT_MANAGER_PATH "input/input_manager.txt"
+#define INPUT_DIRECTORY "input/"
 #define OUTPUT_MANAGER_PATH "output/output_names.txt"
 #define OUTPUT_DIRECTORY "output/"
 
